cc76a4e961603c00dbc4effe094462fa4518f5a2
ThothathriG <thothathrig@rki.de>
2025-05-26 08:33:49 +0200

    changes_Postgresql

--- create_puf_tables_postgres.sql
+++ create_puf_tables_postgres.sql
@@ -318,33 +318,34 @@ CREATE TABLE "khentg" (
     "DATENMODELL" SMALLINT NOT NULL          -- Data model ID
 );
 
-CREATE TABLE icd10_catalogue (
-    kapitelnummer VARCHAR(5),            -- e.g., 3
-    klassart VARCHAR(5),                 -- e.g., N = Normal, T = Terminal
-    dim1 VARCHAR(5),                     -- e.g., X
-    dim2 VARCHAR(5),                     -- e.g., 1
-    dreisteller VARCHAR(10),            -- e.g., A00
-    viersteller_mit_punkt VARCHAR(10),  -- e.g., A00.-
-    viersteller_ohne_punkt VARCHAR(10), -- e.g., A00
-    schlüsselnummer VARCHAR(10),        -- e.g., A00
-    titel_lang TEXT,                    -- e.g., "Cholera durch Vibrio..."
-    titel_kurz TEXT,                    -- e.g., Cholera
-    hinweis TEXT,                       -- usually empty
-    excludes TEXT,                      -- usually empty
-    geschlecht_1 VARCHAR(5),            -- e.g., V
-    geschlecht_2 VARCHAR(5),            -- e.g., V
-    dim1_kode_1 VARCHAR(10),            -- e.g., 1-002
-    dim1_kode_2 VARCHAR(10),            -- e.g., 2-001
-    dim1_kode_3 VARCHAR(10),            -- e.g., 3-003
-    dim1_kode_4 VARCHAR(10),            -- e.g., 4-002
-    alter_von VARCHAR(5),               -- e.g., 1
-    alter_bis VARCHAR(5),               -- e.g., 9
-    alter_einheit VARCHAR(5),           -- e.g., 9
-    gueltig_von VARCHAR(10),            -- e.g., 9999
-    gueltig_bis VARCHAR(10),            -- e.g., 9999
-    kennzeichen VARCHAR(5),             -- e.g., 9
-    dim2_kode_1 VARCHAR(5),             -- e.g., J
-    dim2_kode_2 VARCHAR(5),             -- e.g., J
-    dim2_kode_3 VARCHAR(5),             -- e.g., J
-    dim2_kode_4 VARCHAR(5)              -- e.g., J
+CREATE TABLE "icd10_catalogue" (
+    "KAPITELNUMMER" VARCHAR(5),            -- e.g., 3
+    "KLASSART" VARCHAR(5),                 -- e.g., N = Normal, T = Terminal
+    "DIM1" VARCHAR(5),                     -- e.g., X
+    "DIM2" VARCHAR(5),                     -- e.g., 1
+    "DREISTELLER" VARCHAR(10),            -- e.g., A00
+    "VIERSTELLER_MIT_PUNKT" VARCHAR(10),  -- e.g., A00.-
+    "VIERSTELLER_OHNE_PUNKT" VARCHAR(10), -- e.g., A00
+    "SCHLÜSSELNUMMER" VARCHAR(10),        -- e.g., A00
+    "TITEL_LANG" TEXT,                    -- e.g., "Cholera durch Vibrio..."
+    "TITEL_KURZ" TEXT,                    -- e.g., Cholera
+    "HINWEIS" TEXT,                       -- usually empty
+    "EXCLUDES" TEXT,                      -- usually empty
+    "GESCHLECHT_1" VARCHAR(5),            -- e.g., V
+    "GESCHLECHT_2" VARCHAR(5),            -- e.g., V
+    "DIM1_KODE_1" VARCHAR(10),            -- e.g., 1-002
+    "DIM1_KODE_2" VARCHAR(10),            -- e.g., 2-001
+    "DIM1_KODE_3" VARCHAR(10),            -- e.g., 3-003
+    "DIM1_KODE_4" VARCHAR(10),            -- e.g., 4-002
+    "ALTER_VON" VARCHAR(5),               -- e.g., 1
+    "ALTER_BIS" VARCHAR(5),               -- e.g., 9
+    "ALTER_EINHEIT" VARCHAR(5),           -- e.g., 9
+    "GUELTIG_VON" VARCHAR(10),            -- e.g., 9999
+    "GUELTIG_BIS" VARCHAR(10),            -- e.g., 9999
+    "KENNZEICHEN" VARCHAR(5),             -- e.g., 9
+    "DIM2_KODE_1" VARCHAR(5),             -- e.g., J
+    "DIM2_KODE_2" VARCHAR(5),             -- e.g., J
+    "DIM2_KODE_3" VARCHAR(5),             -- e.g., J
+    "DIM2_KODE_4" VARCHAR(5)              -- e.g., J
 );
+
