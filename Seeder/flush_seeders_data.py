00d837a5e01670e20108f488d21ee271b1a9aed5
gokulethirajanT <gokulethirajan@gmail.com>
2025-07-08 00:52:17 +0200

    docs/bugs:flush + seeder bugs

---
+++ Seeder/flush_seeders_data.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+TABLES = [
+    "zahnleist", "zahnbef", "zahnfall",
+    "khentg", "khproz", "khfa", "khdiag", "khfall",
+    "ezd", "rez",
+    "ambops", "ambleist", "ambdiag", "ambfall",
+    "versqdmp", "versq", "vers"
+]
+
+conn = psycopg2.connect(
+    dbname="CORE_MASTER_THESIS",
+    user="postgres",
+    password="London@123",
+    host="localhost",
+    port="5432"
+)
+
+cur = conn.cursor()
+cur.execute(f'TRUNCATE TABLE {", ".join(TABLES)} RESTART IDENTITY CASCADE;')
+conn.commit()
+cur.close()
+conn.close()
+
+print("✅ All data flushed successfully.")
